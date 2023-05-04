77ed19c74d0ab6d3eff296cae95bd97f1679fa4a
Onaicat <taciano.levi@gmail.com>
2023-05-03 23:41:53 -0300

    url_views

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+# from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME 1')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
