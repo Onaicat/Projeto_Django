6cc26ebd5bc7d4f167ca160509795f7608807768
Onaicat <taciano.levi@gmail.com>
2023-05-04 22:09:08 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'global/home.html', context={
         'name': 'Taciano Levi',
     })
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
